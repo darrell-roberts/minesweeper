971b672f6a3f168ccd8e25ba8ea7277172bbac8c
Darrell Roberts <droberts0674@gmail.com>
2023-01-24 19:38:26 -0500

    update styles

--- vite-minesweeper/src/App.tsx
+++ vite-minesweeper/src/App.tsx
@@ -5,9 +5,14 @@ import { GameState, OpenResult, Position } from "./common/types";
 import CellComp from "./components/Cell/Cell";
 import { message } from '@tauri-apps/api/dialog';
 
+type GameAppState = {
+    board: Position[],
+    state: GameState,
+}
+
 function App() {
-    const [board, setBoard] = useState<Position[]>([]);
-    const [gameState, setGameState] = useState<GameState>("New");
+    const [gameState, setGameState] = useState<GameAppState>({ board: [], state: "New" });
+    const gameActive = gameState.state === "Active" || gameState.state === "New";
 
     useEffect(() => {
         newGame()
@@ -15,38 +20,37 @@ function App() {
 
     useEffect(() => {
         if (!gameActive) {
-            message(gameState, "Game Status")
+            message(gameState.state, "Game Status")
                 .catch((err) => console.error("Failed to open dialog", err));
         }
-    }, [gameState]);
+    }, [gameState.state]);
 
     async function openCell(position: Position) {
         if (position.cell.state.type === "Closed") {
             const result = await invoke<OpenResult>("open", { position });
-            setGameState(result.gameState);
 
-            const updatedBoard = [...board];
+            const updatedBoard = [...gameState.board];
             for (const opened of result.openedCells) {
                 updatedBoard[opened.index].cell = opened.cell
             }
-            setBoard(updatedBoard);
+            setGameState({
+                board: updatedBoard,
+                state: result.gameState
+            });
         }
     }
 
     function newGame() {
         invoke<Position[]>("new_game")
-            .then(setBoard)
-            .then(() => setGameState("New"))
+            .then(board => setGameState({ board, state: "New" }))
             .catch(err => console.error("Failed to start game", err));
     }
 
-    const gameActive = gameState === "Active" || gameState === "New";
-
     return (
         <div className="App">
-            {board.length > 0 &&
+            {gameState.board.length > 0 &&
                 <div className={`board ${!gameActive ? "gameOver" : ""}`}>
-                    {board.map(cell => <CellComp position={cell} open={openCell} gameActive={gameActive} />)}
+                    {gameState.board.map(cell => <CellComp position={cell} open={openCell} gameActive={gameActive} />)}
                 </div>
             }
             <button className="newGame" onClick={() => newGame()} >
